e5a7ca6177eb3e3184eb9ba14e2258c250d3f233
akashzone <akashnadar.dev@gmail.com>
2026-06-15 20:18:23 +0530

    feat(course): fetch courses from API (GET - /courses) and render them in My Courses page

--- frontend/src/pages/instructor/MyCourses.jsx
+++ frontend/src/pages/instructor/MyCourses.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CourseCard from '../../components/CourseCard'
+import { useAuth } from "../../context/AuthContext";
+import api from '../../api/api';
+import { useNavigate } from "react-router-dom";
 
 const MyCourses = () => {
+    const { token } = useAuth();
+    const [courses, setCourses] = useState([]);
+    useEffect(() => {
+        const fetchCourses = async () => {
+            try {
+                const res = await api.get("/courses", {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                console.log("Response :", res.data.courses)
+                setCourses(res.data.courses);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        if (token) {
+            fetchCourses();
+        }
+    }, [token]);
     return (
         <div>
             <h1>My Courses</h1>
+
+            {
+                courses?.map((course) => {
+                    return <CourseCard key={course._id}
+                        title={course.title}
+                        price={course.price}
+                        description={course.description}
+                        category={course.category}
+                        level={course.level}
+                    ></CourseCard>
+                })
+            }
             <CourseCard
                 title="Advanced JavaScript Essentials"
                 price={899}
