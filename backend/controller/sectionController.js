f3a2d8ebdfaf760e2458ae5f857664bd04d6a283
akashzone <akashnadar.dev@gmail.com>
2026-06-17 11:32:30 +0530

    feat(section): add instructor ownership validation for section creation

--- backend/controller/sectionController.js
+++ backend/controller/sectionController.js
@@ -1,7 +1,9 @@
 const Section = require("../models/Section");
+const Course = require("../models/Course");
 
 const createSection = async (req, res) => {
   const { title, course, order } = req.body;
+  const courseId = course;
   console.log("Title :", title);
   if (!title || !course || !order) {
     return res.status(401).json({
@@ -9,15 +11,40 @@ const createSection = async (req, res) => {
       message: "All fields are required",
     });
   }
-  try{
+  try {
+    const course = await Course.findById(courseId);
+    if (!course) {
+      console.error("Error id is not valid :) so can't find the course.");
+      res.status(500).json({ message: "Error id is not valid :)" });
+    }
+    const courseInstructor = course.instructor;
+    console.log("Course instrcutor :", courseInstructor);
+    console.log("req.user.UserId :", req.user.id);
+
+    //Here I'm implementing Ownership - bcz other instructor (Instructor B),
+    // shouldn't make any changes in courses of (Instructor A).
+    if (courseInstructor.toString() !== req.user.id) {
+      return res.status(401).json({
+        status: false,
+        message: "Course cannot be accessed by another instructor",
+      });
+    }
     const sectionInfo = new Section({
-        title,
-        course,
-        order
+      title,
+      course,
+      order,
     });
     await sectionInfo.save();
-    console.log("sectionInfo is successfully saved in DB! Data - ",sectionInfo);
-  }catch(error){
+    console.log(
+      "sectionInfo is successfully saved in DB! Data - ",
+      sectionInfo,
+    );
+    res.status(201).json({
+      section: sectionInfo,
+      success: true,
+      message: "Section created successfully.",
+    });
+  } catch (error) {
     console.error("Error in storing sectionInfo in DB:", error);
     res.status(500).json({ message: "Server error" });
   }
