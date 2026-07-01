c1facbe62c02e27ed28338609f83ced81ba44881
akashzone <akashnadar.dev@gmail.com>
2026-07-01 19:25:50 +0530

    made changes in the base_URL in frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // Use the environment variable for the base URL
   headers: {
     "Content-Type": "application/json",
   },
